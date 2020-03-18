05178a91a461768f8579d22999f77112dffe60a9
Alex Rush <sasha.rush@gmail.com>
2020-03-18 09:46:03 -0400

    .

--- main.py
+++ main.py
@@ -8,18 +8,15 @@ from flask import Flask, render_template, render_template_string
 
 from flask_frozen import Freezer
 import pickle
+import os.path
+from os import path
+
 
 # https://openreview.net/group?id=ICLR.cc/2020
 def prerender_jinja(text):
     """ Pre-renders Jinja templates before markdown. """
     prerendered_body = render_template_string(text)
     return prerendered_body
-
-freezer = Freezer(app, with_no_argument_rules=False, log_url_for=False)
-
-
-import os.path
-from os import path
 
 
 if path.exists("cached_or"):
@@ -29,26 +26,26 @@ else:
     import openreview
     c = openreview.Client(baseurl='https://openreview.net')
     notes = c.get_notes(invitation='ICLR.cc/2020/Conference/-/Blind_Submission') + \
-                                    c.get_notes(invitation='ICLR.cc/2020/Conference/-/Blind_Submission', offset=1000, limit=1000) + \
-                                    c.get_notes(invitation='ICLR.cc/2020/Conference/-/Blind_Submission', offset=2000, limit=1000)
+            c.get_notes(invitation='ICLR.cc/2020/Conference/-/Blind_Submission',
+                        offset=1000, limit=1000) + \
+            c.get_notes(invitation='ICLR.cc/2020/Conference/-/Blind_Submission',
+                        offset=2000, limit=1000)
     pickle.dump(notes, open("cached_or", "bw"))
 
-# @app.route('/')
-# def page():
-#     return str(notes[0])
 
 @app.route('/poster_<poster>.html')
 def poster(poster):
 
     node_id = int(poster)
     print(poster, notes[node_id])
-    data = {"openreview": notes[node_id], "id": node_id, "next": node_id +1 , "prev": node_id-1}
+    data = {"openreview": notes[node_id], "id": node_id,
+            "next": node_id +1 , "prev": node_id-1}
     print(data)
     if "TL;DR" in data["openreview"].content:
         data["openreview"].content["TLDR"] = data["openreview"].content["TL;DR"]
     return render_template('pages/page.html', **data)
 
-
+freezer = Freezer(app, with_no_argument_rules=False, log_url_for=False)
 @freezer.register_generator
 def your_generator_here():
     for i in range(1, 10):
