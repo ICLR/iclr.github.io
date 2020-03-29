35542780149006f0af846f1bb1f5474eb517dd4a
Alex Rush <sasha.rush@gmail.com>
2020-03-29 12:24:55 -0400

    paper embeddings

--- recommendations/get_authors.py
+++ recommendations/get_authors.py
@@ -9,8 +9,9 @@ client = openreview.Client(baseurl='https://openreview.net')
 
 authors = set()
 for y in ["2017", "2018", "2019", "2020"]:
-    blind_notes = {note.id: note for note in openreview.tools.iterget_notes(client,
-                                                                            invitation = 'ICLR.cc/2020/Conference/-/Blind_Submission', details='original')}
+    blind_notes = {note.id: note
+                   for note in openreview.tools.iterget_notes(client,
+                                                              invitation = 'ICLR.cc/2020/Conference/-/Blind_Submission', details='original')}
     for k, v in blind_notes.items():
         for author in v.content["authors"]:
             authors.add(author)
