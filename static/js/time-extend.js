2cc3fc11427a46e1d53bdd40106e53270d3e94d8
Alex Rush <sasha.rush@gmail.com>
2020-04-23 17:52:10 -0400

    update keywords and timezones

--- static/js/time-extend.js
+++ static/js/time-extend.js
@@ -12,8 +12,8 @@ add_local_tz = selector  => {
             // console.log(start_time,end_time,"--- start_time,end_time");
             // console.log(start_time.tz(guess_tz).format('h:mm A'),"--- start_time.");
             // console.log(end_time.tz(guess_tz).format('h:mm A z'),"--- start_time.");
-            const local_start = start_time.tz(guess_tz).format('h:mm A');
-            const local_end_and_tz = end_time.tz(guess_tz).format('h:mm A z');
+            const local_start = start_time.tz(guess_tz).format('HH:mm');
+            const local_end_and_tz = end_time.tz(guess_tz).format('HH:mm z');
             $(this).text(`(${res[1]}-${res[2]} ${res[3]} / ${local_start}-${local_end_and_tz})`)
 
         }
